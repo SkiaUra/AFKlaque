6aa3563b8ea36f6278a0fb2aa7678d2046f9b9a9
Eddy <eddy.margueron@gmail.com>
2022-05-22 17:52:00 +0200

    :brain: Create Logic Decision based on table matrix

--- Assets/Scripts/FSM/State_BasicAttack.cs
+++ Assets/Scripts/FSM/State_BasicAttack.cs
@@ -3,32 +3,29 @@ using DG.Tweening;
 
 [CreateAssetMenu(fileName = "State/BasicAttack")]
 public class State_BasicAttack : BaseState {
+
     Tween Tween;
+
     public override void EnterState(FighterSM _FighterSM) { // Do enter shit once
-        //Debug.LogWarning("Entering BasicAttack");
-        Vector3 TargetPosition = ClosestPositionToTarget(_FighterSM);
-        Tween = _FighterSM.move.MoveToPosition(_FighterSM, TargetPosition);
+
     }
 
     public override void UpdateState(FighterSM _FighterSM) { // Do things
-        if (!Tween.IsPlaying()) {
-            // check distance, if attack is needed
-            Debug.LogWarning("!!! ATTACK !!!");
-            float dist = Vector3.Distance(_FighterSM.FighterEntity.transform.position, _FighterSM.FighterEntity.EnemyFighter.transform.position);
-            if (Mathf.Abs(dist - _FighterSM.FighterEntity.MainWeapon.AttackRange) <= 0.1f) {
-                // HIT
-                _FighterSM.FighterEntity.EnemyFighter.ComputedCurrentHealth -= _FighterSM.FighterEntity.MainWeaponDamage;
-                Debug.Log("=> Attack Hit !!");
-            } else {
-                // FAIL
-                Debug.Log("=> Attack Fail !!");
-            }
-            _FighterSM.SwitchState(_FighterSM.move);
+        Debug.LogWarning("!!! ATTACK !!!");
+        float dist = Vector3.Distance(_FighterSM.FighterEntity.transform.position, _FighterSM.FighterEntity.EnemyFighter.transform.position);
+        if (Mathf.Abs(dist - _FighterSM.FighterEntity.MainWeapon.AttackRange) <= 0.1f) {
+            // HIT
+            _FighterSM.FighterEntity.EnemyFighter.ComputedCurrentHealth -= _FighterSM.FighterEntity.MainWeaponDamage;
+            Debug.Log("=> Attack Hit !!");
+        } else {
+            // FAIL
+            Debug.Log("=> Attack Fail !!");
         }
+        ExitState(_FighterSM);
     }
 
     public override void ExitState(FighterSM _FighterSM) { // End things if needed
-
+        _FighterSM.MakeNewDecision();
     }
 
     public Vector3 ClosestPositionToTarget(FighterSM _FighterSM) {
